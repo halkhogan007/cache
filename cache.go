33901f8c19907d031614896be4d2c495b3fb7bc8
user_417_4272 <bpo@oac.gov.by>
2023-01-05 10:38:38 +0300

    final version

--- cache.go
+++ cache.go
@@ -4,30 +4,31 @@ import (
 	"errors"
 )
 
-type cache struct {
+type Cache struct {
 	memory map[string]interface{}
 }
 
-func New() *cache {
-	return &cache{
+func New() *Cache {
+	return &Cache{
 		memory: make(map[string]interface{}),
 	}
 }
-func (c *cache) Set(key string, value interface{}) error {
+
+func (c *Cache) Set(key string, value interface{}) error {
 	if len(key) == 0 {
 		return errors.New("Invalid key")
 	}
 	c.memory[key] = value
 	return nil
 }
-func (c cache) Get(key string) (interface{}, error) {
-	if c.memory[key] == 0 {
-		return errors.New("unknown key for get."), nil
+func (c *Cache) Get(key string) (interface{}, error) {
+	if c.memory[key] == nil {
+		return nil, errors.New("unknown key for get.")
 	}
 	return c.memory[key], nil
 }
-func (c *cache) Delete(key string) error {
-	if c.memory[key] == 0 {
+func (c *Cache) Delete(key string) error {
+	if c.memory[key] == nil {
 		return errors.New("unknown key for delete.")
 	}
 	delete(c.memory, key)
