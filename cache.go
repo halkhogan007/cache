3782ca97f30d9169067c1f5ac4852d31381aec85
user_417_4272 <bpo@oac.gov.by>
2023-01-05 10:05:53 +0300

    debug

--- cache.go
+++ cache.go
@@ -8,12 +8,12 @@ type cache struct {
 	memory map[string]interface{}
 }
 
-func New() cache {
-	return cache{
+func New() *cache {
+	return &cache{
 		memory: make(map[string]interface{}),
 	}
 }
-func (c cache) Set(key string, value interface{}) error {
+func (c *cache) Set(key string, value interface{}) error {
 	if len(key) == 0 {
 		return errors.New("Invalid key")
 	}
@@ -26,7 +26,7 @@ func (c cache) Get(key string) (interface{}, error) {
 	}
 	return c.memory[key], nil
 }
-func (c cache) Delete(key string) error {
+func (c *cache) Delete(key string) error {
 	if c.memory[key] == 0 {
 		return errors.New("unknown key for delete.")
 	}
